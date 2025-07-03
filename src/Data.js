107fd9771b832d9cb32b71805cca10871bc48e88
arham <arhambutt2923@gmail.com>
2025-07-03 23:55:14 +0500

    data added

---
+++ src/Data.js
@@ -0,0 +1,15 @@
+import web from "./images/swords.png"
+import ui_ux from "./images/ui.webp"
+import api from "./images/api.jpeg"
+import anime from "./images/animated-removebg-preview.png"
+import bug from "./images/bug-removebg-preview.png"
+
+
+const data = [
+    { name: " Frontend Web Development", img: { web }, info: "I build responsive, interactive websites using React and Tailwind, focusing on speed, performance, and clean code" },
+    { name: " Animated User Interfaces", img: { anime }, info: "I add life to websites with Framer Motion animations — scroll, hover, and reveal effects that create memorable experiences" },
+    { name: " REST API Integration", img: { api }, info: "I connect frontend apps to real-time data with clean API integration, handling both logic and UI smoothly" },
+    { name: " Custom UI Components", img: { ui_ux }, info: "I create reusable, scalable components — buttons, modals, navbars — designed to fit your brand and keep code maintainable" },
+    { name: "Bug Fixing & Code Optimization", img: { bug }, info: "Already have a site? I help improve layout, fix responsiveness, remove clutter, and optimize performance under the hood" },
+]
+export default data
--- src/Services.jsx
+++ src/Services.jsx
@@ -1,8 +1,12 @@
 import React, { useContext } from 'react'
 import { App_context } from './Context'
+import data from './Data'
 
 function Services() {
     let { drk_mod, cursor_effect_on_leave, cursor_effect_on_enter } = useContext(App_context)
+
+
+
     return (
         <div className='w-full  py-10'>
             <h1 className={`font-bold textanime ${drk_mod ? "text-white ts_W" : "text-black ts_b"} text-xl sm:text-3xl md:text-5xl text-center -mt-2 sm:mt-5 md:mt-13 transition-all duration-200 ease-in`}>🎯 <span className="text-yellow-400 italic underline ts_y">Services</span> I Provide </h1>
@@ -10,23 +14,31 @@ function Services() {
                 From crafting sleek, user-friendly interfaces to designing playful, interactive animations — I specialize in transforming your bold ideas into seamless, responsive digital experiences. Every pixel I place has a purpose, and every motion I create adds meaning. Whether you're building a brand or upgrading your web presence, I’m here to turn your vision into an engaging, functional reality — pixel by pixel.
             </p>
             <div className='w-full h-max flex justify-center items-center gap-10 flex-wrap  mt-20'>
-               
-                <div className=" w-max h-max" style={{ perspective: '1000px' }}>
-                    <div onMouseMove={cursor_effect_on_enter} onMouseLeave={cursor_effect_on_leave}
-                        className="w-[400px] h-[500px] rounded-2xl shadow-2xl transition-all ease-out duration-200"
-                        style={{
-                            transformStyle: 'preserve-3d',
-                            color: 'white',
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            border: '5px solid yellow'
-                        }}
-                    >
 
-                    </div>
-                </div>
-                
+                {data.map((ele, idx) => {
+                    return (
+
+                        <div key={idx} className=" w-max h-max" style={{ perspective: '1000px' }}>
+                            <div onMouseMove={cursor_effect_on_enter} onMouseLeave={cursor_effect_on_leave}
+                                className="w-[350px] h-[400px] rounded-2xl shadow-2xl transition-all ease-out duration-200"
+                                style={{
+                                    transformStyle: 'preserve-3d',
+                                    color: 'white',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    border: '5px solid yellow'
+                                }}
+                            >
+                                <div className='w-[100px] h-[100px] border'><img src={ele.img} alt="" /></div>
+
+                            </div>
+                        </div>
+                    )
+                })}
+
+
+
             </div>
             {/* <p>5 cards remaing</p> */}
             {/* <p>Frontend Web Development</p> */}
