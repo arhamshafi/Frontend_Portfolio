1e7c8b86c43ff64bed5082e2f42c43743e18e660
arham <arhambutt2923@gmail.com>
2025-07-15 22:14:51 +0500

    portfolio section completed

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -5,6 +5,7 @@ import img_2 from "./images/mini_store.png"
 import img_3 from "./images/furniture_web.png"
 import img_4 from "./images/expence_project.png"
 import img_5 from "./images/quizz app.png"
+import { RxCookie } from "react-icons/rx";
 
 function Portfolio() {
     let { drk_mod } = useContext(App_context)
@@ -16,7 +17,7 @@ function Portfolio() {
             </h1>
             <p className={`mt-3 md:mt-16 w-full text-center text-sm md:text-xl textanime ${drk_mod ? "text-gray-300" : "text-gray-700"} px-4 md:px-20 transition-all duration-200 ease-in`}>
                 Explore a curated set of real-world web applications that merge high performance, creative UI, and intuitive design. Scroll down to witness where logic meets creativity </p>
-            <div className='w-full mt-25 '>
+            <div className='w-full mt-10 lg:mt-20 py-1'>
                 {
                     [{
                         id: 1,
@@ -77,7 +78,7 @@ function Portfolio() {
                             "Weather API for city-based temperature display",
                             "Data stored in LocalStorage for persistence"
                         ],
-                        tech: ["React", "Context API", "Tailwind CSS", "OpenWeather API", "Day.js"],
+                        tech: ["React", "Context API", "Tailwind CSS", "OpenWeather API"],
                         img: img_4,
                         live: "https://expense-reviewer-masb.netlify.app/",
                         github: "https://github.com/arhamshafi"
@@ -100,65 +101,105 @@ function Portfolio() {
                     }
                     ].map((ele, idx) => {
                         return (
-                            <div key={idx}>
-                            
-                            <div 
-                                key={idx}
-                                className={`w-[90%] mx-auto hidden sm:flex flex-col h-auto  md:flex-row ${idx % 2 !== 0 ? "md:flex-row-reverse" : ""} prt justify-between items-center mt-10`}
-                            >
-                                {/* Left / Right Image Box */}
-                                <div className={`w-full md:w-[37%]  rounded-xl overflow-hidden shadow-xl  `}>
-                                    {ele.img ? (
-                                        <img
-                                            src={ele.img}
-                                            alt={ele.title}
-                                            className="w-full h-max object-contain"
-                                        />
-                                    ) : (
-                                        <div className="w-full h-full flex items-center justify-center text-gray-500">
-                                            Image Here
-                                        </div>
-                                    )}
-                                </div>
+                            <div key={idx} className='mt-0'>
 
-                                {/* Content Area */}
-                                <div className={`w-full md:w-[60%] ${drk_mod ? "text-white" : "text-black"} p-5 transition-all flex flex-col mt-5 md:mt-0 justify-center duration-200 ease-in rounded-xl h-full space-y-2`}>
-                                    <h2 className={`text-xl font-bold text-yellow-400`}>{ele.title}</h2>
-                                    <p className="text-sm">{ele.desc}</p>
-                                    <ul className={`list-disc list-inside text-sm ${drk_mod ? "text-white/70" : "text-black/70"} transition-all duration-200 ease-in `}>
-                                        {ele.features.map((f, i) => (
-                                            <li key={i}>{f}</li>
-                                        ))}
-                                    </ul>
-                                    <div className="flex flex-wrap gap-2 text-xs pt-2">
-                                        {ele.tech.map((tech, i) => (
-                                            <span key={i} className="bg-yellow-400 text-black px-2 py-1 rounded-full">
-                                                {tech}
-                                            </span>
-                                        ))}
+                                <div
+                                    key={idx}
+                                    className={`w-[90%] mx-auto group hidden sm:flex flex-col h-auto  md:flex-row ${idx % 2 !== 0 ? "md:flex-row-reverse" : ""} prt justify-between items-center mt-10 textanime`}  style={{ animationDelay: `${(idx + 1) * 0.2}s` }}
+                                >
+                                    {/* Left / Right Image Box */}
+                                    <div className={`w-full md:w-[37%]  rounded-xl overflow-hidden shadow-xl  `}>
+                                        {ele.img ? (
+                                            <img
+                                                src={ele.img}
+                                                alt={ele.title}
+                                                className="w-full h-max group-hover:scale-110 scale-105 transition-all duration-200 ease-in object-contain"
+                                            />
+                                        ) : (
+                                            <div className="w-full h-full flex items-center justify-center text-gray-500">
+                                                Image Here
+                                            </div>
+                                        )}
                                     </div>
-                                    <div className="flex gap-3 pt-4">
-                                        <a
-                                            href={ele.live}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            className="px-4 py-1.5 rounded-full text-sm font-medium transition duration-200 border-2 
+
+                                    {/* Content Area */}
+                                    <div className={`w-full md:w-[60%] ${drk_mod ? "text-white" : "text-black"} p-5 transition-all flex flex-col mt-5 md:mt-0 justify-center duration-200 ease-in rounded-xl h-full space-y-2`}>
+                                        <h2 className={`text-xl font-bold ${drk_mod ? "text-yellow-400" : "text-yellow-500"} ts_y`}>{ele.title}</h2>
+                                        <p className="text-sm">{ele.desc}</p>
+                                        <ul className={`list-disc list-inside text-sm ${drk_mod ? "text-white/70" : "text-black/70"} transition-all duration-200 ease-in `}>
+                                            {ele.features.map((f, i) => (
+                                                <li key={i}>{f}</li>
+                                            ))}
+                                        </ul>
+                                        <div className="flex flex-wrap gap-2 text-xs pt-2">
+                                            {ele.tech.map((tech, i) => (
+                                                <span key={i} className={` transition-all duration-150 ease-linear border border-yellow-400 ${drk_mod ? "text-black bg-yellow-400  hover:bg-black hover:text-yellow-500" : "bg-yellow-500 text-white  hover:bg-white hover:text-yellow-600"}  text-black px-2 py-1 rounded-full`}>
+                                                    {tech}
+                                                </span>
+                                            ))}
+                                        </div>
+                                        <div className="flex gap-3 pt-4">
+                                            <a
+                                                href={ele.live}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="px-4 py-1.5 rounded-full text-sm font-medium transition duration-200 border-2 
                border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white  dark:border-blue-400 dark:text-blue-400 dark:hover:bg-blue-400 dark:hover:text-black"
-                                        > 🌐 Live Demo </a>
-                                        <a href={ele.github}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            className="px-4 py-1.5 rounded-full text-sm font-medium transition duration-200 border-2 
+                                            > 🌐 Live Demo </a>
+                                            <a href={ele.github}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="px-4 py-1.5 rounded-full text-sm font-medium transition duration-200 border-2 
                border-gray-700 text-gray-700  hover:bg-gray-700 hover:text-white dark:border-white/60 dark:text-white/70 
                dark:hover:bg-white dark:hover:text-black" >📁 GitHub</a>
+                                        </div>
+
                                     </div>
-
                                 </div>
-                            </div>
+
+                                <div className='w-[90%] mx-auto group rounded-xl bg-cover bg-center overflow-hidden relative h-max flex justify-center items-center sm:hidden mt-5 textanime'  style={{ animationDelay: `${(idx + 1) * 0.2}s` }}
+                                >
+                                    <RxCookie className={`${drk_mod ? "text-white/20" : "text-black/20"} text-8xl absolute textanime`}  style={{ animationDelay: `${(idx + 1) * 0.2}s` }} />
+
+                                    <img src={ele.img} className='absolute top-10  right-2 w-[150px] r_img rounded-2xl z-10' alt="" />
+                                    <div className={`w-full h-full relative p-5 top-0 left-0 ${drk_mod ? "bg-white/20" : "bg-black/10"}`}>
+                                        <div className='w-[70%] h-max r_bb'>
+                                            <h1 className={`${drk_mod ? "text-yellow-400" : "text-yellow-500"} ts_y`}>{ele.title}</h1>
+                                            <ul className={`list-disc list-inside text-[11px] mt-4 ${drk_mod ? "text-white/70" : "text-black/70"} transition-all duration-200 ease-in `}>
+                                                {ele.features.map((f, i) => (
+                                                    <li key={i}>{f}</li>
+                                                ))}
+                                            </ul>
+
+                                        </div>
+                                        <p className={` text-[11px] mt-3 ${drk_mod ? " text-white" : "text-black "} `}>{ele.desc}</p>
+                                        <div className="flex flex-wrap gap-2 text-[10px] pt-2">
+                                            {ele.tech.map((tech, i) => (
+                                                <span key={i} className={` transition-all duration-150 ease-linear border border-yellow-400 hover:bg-transparent ${drk_mod ? "text-black bg-yellow-400   hover:text-yellow-500" : "bg-yellow-500 text-white hover:text-yellow-600"}  text-black px-2 py-1 rounded-full`}>
+                                                    {tech}
+                                                </span>
+                                            ))}
+                                        </div>
+                                        <div className="flex gap-3 pt-4">
+                                            <a
+                                                href={ele.live}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="px-2 py-1 rounded-full text-[10px] font-medium transition duration-200 border-2 
+               border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white  dark:border-blue-400 dark:text-blue-400 dark:hover:bg-blue-400 dark:hover:text-black"
+                                            > 🌐 Live Demo </a>
+                                            <a href={ele.github}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className="px-2 py-1 rounded-full text-[10px] font-medium transition duration-200 border-2 
+               border-gray-500 text-gray-500  hover:bg-gray-500 hover:text-white dark:border-white/40 dark:text-white/40 
+               dark:hover:bg-white dark:hover:text-black" >📁 GitHub</a>
+                                        </div>
+                                    </div>
+                                </div>
 
 
-
-
+                                {/* <div className='w-[200px] h-[100px] rounded-xl mt-3  overflow-hidden'> <img src={ele.img} className='w-full scale-103 transition-all duration-200 ease-in-out h-full group-hover:scale-110' alt="" /> </div> */}
                             </div>
                         )
                     })
